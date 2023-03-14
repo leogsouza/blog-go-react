ed756fe02162c892242482d54d69e4d8809ed6f7
Leonardo Souza <leogsouza@gmail.com>
2023-03-14 14:45:37 -0300

    First commit

---
+++ database/connect.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+"log"
+"os"
+
+"github.com/joho/godotenv"
+"gorm.io/gorm"
+"gorm.io/driver/mysql"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error load .env file")
+	}
+
+	dsn := os.Getenv("DB_DSN")
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+
+	DB = database
+	
+}
